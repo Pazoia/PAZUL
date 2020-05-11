c144f5cab8ab815ed5f674a238bfe8a8e55890b2
Paulo Azoia <pazoia.gomes@gmail.com>
2020-05-11 10:13:28 +0100

    take_random function refactored and working, added to game.py
    refactor to testing in progress

--- game.py
+++ game.py
@@ -2,15 +2,23 @@ from src.tile_collection import TileCollection
 
 class Game():
     def __init__(self):
-        pass
+        self.STARTING_TILES = 20
 
     def new_game(self):
         self.bag = TileCollection()
-        self.bag.add_tiles("blue", 20)
-        self.bag.add_tiles("red", 20)
-        self.bag.add_tiles("orange", 20)
-        self.bag.add_tiles("purple", 20)
-        self.bag.add_tiles("pink", 20)
+        self.bag.add_tiles("blue", self.STARTING_TILES)
+        self.bag.add_tiles("red", self.STARTING_TILES)
+        self.bag.add_tiles("orange", self.STARTING_TILES)
+        self.bag.add_tiles("purple", self.STARTING_TILES)
+        self.bag.add_tiles("pink", self.STARTING_TILES)
+
+        print(self.bag.__dict__)
+
+        self.factory = TileCollection()
+        for i in range(4):
+            self.factory.add_tile(self.bag.take_random())
+        
+        print(self.factory.__dict__)
 
         print(self.bag.__dict__)
 
--- pazul/tile_collection.py
+++ pazul/tile_collection.py
@@ -4,83 +4,39 @@ from pazul.exceptions import EmptyTileCollectionError
 class TileCollection:
     def __init__(self):
         self.tiles = {}
-        self.number_of_tiles = 0
+        self.size = 0
 
     def add_tile(self, color):
         self.tiles[color] = self.tiles.get(color, 0) + 1
-        self.number_of_tiles += 1
+        self.size += 1
 
     def add_tiles(self, color, quantity):
         self.tiles[color] = self.tiles.get(color, 0) + quantity
-        self.number_of_tiles += quantity
+        self.size += quantity
 
     def take_all_of(self, color):
         tiles_to_take = self.tiles.get(color, 0)
         self.tiles[color] = 0
-        self.number_of_tiles = self.number_of_tiles - tiles_to_take
+        self.size = self.size - tiles_to_take
         return tiles_to_take
 
     def take_all(self):
         tiles_to_take = self.tiles
         self.tiles = {}
-        self.number_of_tiles = 0
+        self.size = 0
         return tiles_to_take
 
     def take_random(self):
-        if self.number_of_tiles == 0:
+        if self.size == 0:
             raise EmptyTileCollectionError()
 
-        chosen_number = random.randint(1, self.number_of_tiles)
-        # print(f"chosen number: {chosen_number}")
+        chosen_number = random.randint(1, self.size)
 
         counter = 0
         for color in self.tiles:
             if self.tiles[color] != 0:
                 counter = counter + self.tiles[color]
-                # print(f"color: {color}; counter: {counter}")
                 if chosen_number <= counter:
                     self.tiles[color] = self.tiles[color] - 1
-                    self.number_of_tiles -= 1
-                    # print(f"num tile remaining: {self.number_of_tiles}")
-                    # print(f"remaining tiles: {self.tiles[color]}")
+                    self.size -= 1
                     return color
-
-    def take_random_2(self):
-        all_tiles = []
-
-        for color in self.tiles:
-            times_to_append = self.tiles[color]
-            for i in range(times_to_append):
-                all_tiles.append(color)
-
-        if len(all_tiles) == 0:
-            raise EmptyTileCollectionError()
-
-        chosen_color = random.choice(all_tiles)
-        self.tiles[chosen_color] = self.tiles[chosen_color] - 1
-        return chosen_color
-
-
-# tileCollection = TileCollection()
-# # Adding tiles
-# tileCollection.add_tiles("blue", 1)
-# tileCollection.add_tiles("red", 1)
-# tileCollection.add_tiles("orange", 1)
-# tileCollection.add_tiles("green", 1)
-
-# # Taking random tiles
-# print(f"take random: {tileCollection.take_random()}")
-# print(tileCollection.tiles)
-# print("- - - - - - - - - - - - -")
-# print(f"take random: {tileCollection.take_random()}")
-# print(tileCollection.tiles)
-# print("- - - - - - - - - - - - -")
-# print(f"take random: {tileCollection.take_random()}")
-# print(tileCollection.tiles)
-# print("- - - - - - - - - - - - -")
-# print(f"take random: {tileCollection.take_random()}")
-# print(tileCollection.tiles)
-# print("- - - - - - - - - - - - -")
-# print(f"take random: {tileCollection.take_random()}")
-# print(tileCollection.tiles)
-# print("- - - - - - - - - - - - -")
