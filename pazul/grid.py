852573e6d2558b9178bfaf3b1459696371f9a0dd
Paulo Azoia <pazoia.gomes@gmail.com>
2020-05-17 20:47:31 +0100

    Started working on add_tile function

--- pazul/grid.py
+++ pazul/grid.py
@@ -1,6 +1,6 @@
 class Grid():
     def __init__(self):
-        colors = ["blue", "yellow", "red", "black", "teal"]
+        COLORS = ["blue", "yellow", "red", "black", "teal"]
 
         self.grid = [
             [colors[0], colors[1], colors[2], colors[3], colors[4]],
@@ -14,5 +14,11 @@ class Grid():
         for row in self.grid:
             print(row)
 
+    def add_tile(self, row, color):
+        row = self.grid[row - 1]
+        print(f"selected row = {row}")
+        # returns points scored by tile
+
 grid = Grid()
 grid.printing_grid()
+grid.add_tile(3, "blue")
