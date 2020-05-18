12807e7096e9c4d73f22061fdf406afe3f97a59b
Paulo Azoia <pazoia.gomes@gmail.com>
2020-05-18 19:04:04 +0100

    add_tile scoring 1 point per tile plus an extra point for 1 tile next to it

--- pazul/grid.py
+++ pazul/grid.py
@@ -1,6 +1,7 @@
 class Grid():
     def __init__(self):
         colors = ["blue", "yellow", "red", "black", "teal"]
+        self.score = 0
 
         self.grid = [
             [colors[0], colors[1], colors[2], colors[3], colors[4]],
@@ -16,17 +17,26 @@ class Grid():
 
     def add_tile(self, row, color):
         row = self.grid[row - 1]
+        row_index = self.grid.index(row)
+
         for i in range(len(row)):
             if row[i] == color:
                 row[i] = "filled"
+                self.score += 1
+                if row_index == -1:
+                    pass
+                if self.grid[row_index - 1][i] == "filled":
+                    self.score += 1
+
+        print(f"score = {self.score}")
 
         # resolve the scores, check filled spaces near tile
         # returns points scored by tile
 
 grid = Grid()
-grid.add_tile(1, "red")
+grid.add_tile(3, "blue")
 grid.printing_grid()
-grid.add_tile(2, "red")
+grid.add_tile(4, "teal")
 grid.printing_grid()
-grid.add_tile(3, "red")
+grid.add_tile(5, "black")
 grid.printing_grid()
