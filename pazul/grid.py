e8e7ec9d1d765d31e0c542342b616718ced06224
Paulo Azoia <pazoia.gomes@gmail.com>
2020-05-18 23:01:42 +0100

    resolving vertical scores working,
    adding extra points from tiles above
    adding extra points from tiles below

--- pazul/grid.py
+++ pazul/grid.py
@@ -23,13 +23,23 @@ class Grid():
             if row[i] == color:
                 row[i] = "filled"
                 self.score += 1
+                print("adding 1 point from tile placement")
 
                 while row_index - 1 != -1:
                     row_index -= 1
                     if self.grid[row_index][i] == "filled":
                         self.score += 1
-                else:
-                    break
+                        print("adding extra point from tile above")
+                    else:
+                        break
+                row_index = self.grid.index(row)
+                while row_index + 1 != 5:
+                    row_index += 1
+                    if self.grid[row_index][i] == "filled":
+                        self.score += 1
+                        print("adding extra point from tile below")
+                    else:
+                        break
 
         print(f"score = {self.score}")
 
@@ -37,11 +47,13 @@ class Grid():
         # returns points scored by tile
 
 grid = Grid()
-grid.add_tile(1, "blue")
+grid.add_tile(1, "red")
 grid.printing_grid()
-grid.add_tile(2, "teal")
+grid.add_tile(2, "yellow")
 grid.printing_grid()
-grid.add_tile(3, "black")
+grid.add_tile(5, "black")
 grid.printing_grid()
-grid.add_tile(4, "red")
+grid.add_tile(4, "teal")
+grid.printing_grid()
+grid.add_tile(3, "blue")
 grid.printing_grid()
