76bc1f902b58eac2da99352fab825349782bac40
Paulo Azoia <pazoia.gomes@gmail.com>
2020-05-18 13:27:26 +0100

    add_tile function marking chosen color in row as filled

--- pazul/grid.py
+++ pazul/grid.py
@@ -1,6 +1,6 @@
 class Grid():
     def __init__(self):
-        COLORS = ["blue", "yellow", "red", "black", "teal"]
+        colors = ["blue", "yellow", "red", "black", "teal"]
 
         self.grid = [
             [colors[0], colors[1], colors[2], colors[3], colors[4]],
@@ -16,9 +16,17 @@ class Grid():
 
     def add_tile(self, row, color):
         row = self.grid[row - 1]
-        print(f"selected row = {row}")
+        for i in range(len(row)):
+            if row[i] == color:
+                row[i] = "filled"
+
+        # resolve the scores, check filled spaces near tile
         # returns points scored by tile
 
 grid = Grid()
+grid.add_tile(1, "red")
 grid.printing_grid()
-grid.add_tile(3, "blue")
+grid.add_tile(2, "red")
+grid.printing_grid()
+grid.add_tile(3, "red")
+grid.printing_grid()
