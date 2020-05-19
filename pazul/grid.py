1d168fcbc959267978e37b63b98f43216356bc02
Paulo Azoia <pazoia.gomes@gmail.com>
2020-05-19 01:22:19 +0100

    add_tile adding horizontal scores

--- pazul/grid.py
+++ pazul/grid.py
@@ -24,6 +24,11 @@ class Grid():
                 row[i] = "filled"
                 self.score += 1
                 print("adding 1 point from tile placement")
+                element_index = i
+
+                if (self.grid[row_index][i - 1] == "filled" or self.grid[row_index][i + 1] == "filled") and (self.grid[row_index - 1][i] == "filled" or self.grid[row_index + 1][i] == "filled"):
+                    self.score += 1
+                    print("adding horizontal tile placement point")
 
                 while row_index - 1 != -1:
                     row_index -= 1
@@ -32,12 +37,35 @@ class Grid():
                         print("adding extra point from tile above")
                     else:
                         break
+                
                 row_index = self.grid.index(row)
                 while row_index + 1 != 5:
                     row_index += 1
                     if self.grid[row_index][i] == "filled":
                         self.score += 1
                         print("adding extra point from tile below")
+                    else:
+                        break
+                
+                row_index = self.grid.index(row)
+                # if self.grid[row_index][i - 1] == "filled" or self.grid[row_index][i + 1] == "filled":
+                #     self.score += 1
+                #     print("adding horizontal tile placement point")
+
+                while i - 1 != -1:
+                    i -= 1
+                    if self.grid[row_index][i] == "filled":
+                        self.score += 1
+                        print("adding extra point from left tile")
+                    else:
+                        break
+
+                i = element_index
+                while i + 1 != 5:
+                    i += 1
+                    if self.grid[row_index][i] == "filled":
+                        self.score += 1
+                        print("adding extra point from right tile")
                     else:
                         break
 
@@ -47,13 +75,17 @@ class Grid():
         # returns points scored by tile
 
 grid = Grid()
-grid.add_tile(1, "red")
-grid.printing_grid()
-grid.add_tile(2, "yellow")
+grid.add_tile(4, "teal")
 grid.printing_grid()
 grid.add_tile(5, "black")
 grid.printing_grid()
-grid.add_tile(4, "teal")
+grid.add_tile(3, "yellow")
+grid.printing_grid()
+grid.add_tile(3, "red")
 grid.printing_grid()
 grid.add_tile(3, "blue")
 grid.printing_grid()
+# grid.add_tile(3, "black")
+# grid.printing_grid()
+# grid.add_tile(3, "blue")
+# grid.printing_grid()
