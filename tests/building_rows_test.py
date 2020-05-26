75dae45a88e6af1d925c82c7e69d1f48c0d724d8
Paulo Azoia <pazoia.gomes@gmail.com>
2020-05-26 15:31:25 +0100

    added test_add_one_tile_to_non_empty_row

--- tests/building_rows_test.py
+++ tests/building_rows_test.py
@@ -1,6 +1,6 @@
 from pazul.building_rows import BuildingRows
 
-def test_add_tile_to_an_empty_row():
+def test_add_one_tile_to_an_empty_row():
     building_rows = BuildingRows()
     building_rows.add_tiles_to_building_row(1, "blue", 1)
     assert building_rows.building_rows[0][-1] == "blue"
@@ -10,3 +10,8 @@ def test_add_many_tiles_to_empty_row():
     building_rows.add_tiles_to_building_row(3, "blue", 3)
     assert building_rows.building_rows[2].count("blue") == 3
 
+def test_add_one_tile_to_non_empty_row():
+    building_rows = BuildingRows()
+    building_rows.add_tiles_to_building_row(2, "blue", 1)
+    building_rows.add_tiles_to_building_row(2, "blue", 1)
+    assert building_rows.building_rows[1].count("blue") == 2
