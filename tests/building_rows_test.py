804ef6ab87e02a236985be2fb90b583c77b623f3
Paulo Azoia <pazoia.gomes@gmail.com>
2020-05-26 16:00:37 +0100

    Added test_add_tile_of_different_color_to_color_in_row

--- tests/building_rows_test.py
+++ tests/building_rows_test.py
@@ -1,4 +1,6 @@
+import pytest
 from pazul.building_rows import BuildingRows
+from pazul.exceptions import InvalidColor
 
 def test_add_one_tile_to_an_empty_row():
     building_rows = BuildingRows()
@@ -26,4 +28,9 @@ def test_too_many_tiles_for_row_size_returning_tiles_not_used():
     building_rows = BuildingRows()
     building_rows.add_tiles_to_building_row(4, "blue", 1)
     assert building_rows.add_tiles_to_building_row(4, "blue", 5) == 2
-    
+
+def test_add_tile_of_different_color_to_color_in_row():
+    with pytest.raises(InvalidColor):
+        building_rows = BuildingRows()
+        building_rows.add_tiles_to_building_row(4, "blue", 3)
+        building_rows.add_tiles_to_building_row(4, "red", 1)
