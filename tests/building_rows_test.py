aa938dbf048be93adf84ebe19ecebacce36150b0
Paul McCabe <phmccabe@googlemail.com>
2020-05-26 17:59:22 +0100

    Alternate implementation with no loop when adding tiles.

--- tests/building_rows_test.py
+++ tests/building_rows_test.py
@@ -2,27 +2,27 @@ import pytest
 from pazul.building_rows import BuildingRows
 from pazul.exceptions import InvalidColor
 
-def test_add_one_tile_to_an_empty_row():
+def test_add_tile_to_an_empty_row_sets_its_color():
     building_rows = BuildingRows()
     building_rows.add_tiles_to_building_row(1, "blue", 1)
-    assert building_rows.building_rows[0][-1] == "blue"
+    assert building_rows.building_rows[0].get_color() == "blue"
 
 def test_add_many_tiles_to_empty_row():
     building_rows = BuildingRows()
     building_rows.add_tiles_to_building_row(3, "blue", 3)
-    assert building_rows.building_rows[2].count("blue") == 3
+    assert building_rows.building_rows[2].filled_slots == 3
 
 def test_add_one_tile_to_non_empty_row():
     building_rows = BuildingRows()
     building_rows.add_tiles_to_building_row(2, "blue", 1)
     building_rows.add_tiles_to_building_row(2, "blue", 1)
-    assert building_rows.building_rows[1].count("blue") == 2
+    assert building_rows.building_rows[1].filled_slots == 2
 
 def test_add_many_tiles_to_non_empty_row():
     building_rows = BuildingRows()
     building_rows.add_tiles_to_building_row(4, "blue", 1)
     building_rows.add_tiles_to_building_row(4, "blue", 3)
-    assert building_rows.building_rows[3].count("blue") == 4
+    assert building_rows.building_rows[3].filled_slots == 4
 
 def test_too_many_tiles_for_row_size_returning_tiles_not_used():
     building_rows = BuildingRows()
